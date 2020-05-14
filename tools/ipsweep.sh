785262353b706b13155dfb1a5b4a17e0145a8fe1
Lee <leetheperm@gmail.com>
2020-05-14 11:03:20 +0100

    edited isweep

--- tools/ipsweep.sh
+++ tools/ipsweep.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# if no command argument is given, a set of instructions will help you
+
 if ["$1" == ""]
 then 
 echo "You need to provide an IP address"
@@ -7,6 +9,7 @@ echo "Example : ./ipsweep.sh 193.189.0"
 
 else 
 
+# scan all 255 ports and ping each one to see which ones respond
 
 for ip in {1..254}; do
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
